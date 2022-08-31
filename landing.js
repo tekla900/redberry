6a5dfb9261cc767e228615a08bee861ed628b492
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-08-31 18:39:15 +0400

    added landing page

---
+++ landing.js
@@ -0,0 +1,7 @@
+document.getElementById("add-note").onclick = function () {
+    location.href = "./redberry/add-note.html";
+};
+
+document.getElementById("see-notes").onclick = function () {
+    location.href = "./redberry/see-notes";
+};
