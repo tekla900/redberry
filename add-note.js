5819a142852c98c876dd850be6718930ff6d33be
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-05 03:39:58 +0400

    sending formdata with POST

--- add-note.js
+++ add-note.js
@@ -3,10 +3,10 @@ const nameInput = document.getElementById('name');
 const surnameInput = document.getElementById('lastname');
 const emailInput = document.getElementById('email');
 const numberInput = document.getElementById('number');
+const teamsSelect = document.getElementById('teams');
+const posSelect = document.getElementById('positions')
 
 const laptopName = document.getElementById('laptop-name');
-// const laptopBrand = document.getElementById('laptop-brands');
-// const laptopCpu =  document.getElementById('cpus');
 const cpuCore = document.getElementById('cpu-core');
 const cpuThread = document.getElementById('cpu-thread');
 const ram = document.getElementById('ram');
@@ -16,22 +16,15 @@ const price = document.getElementById('price');
 
 const form = document.querySelector('form');
 
-if(!localStorage.getItem('name')) {
-  populateStorage();
-} else {
-  setInputs();
-}
 
+// saving to local storage
 function populateStorage() {
   localStorage.setItem('name',  nameInput.value);
   localStorage.setItem('surname',  surnameInput.value);
   localStorage.setItem('email',  emailInput.value);
   localStorage.setItem('number',  numberInput.value);
 
-   
   localStorage.setItem('laptop-name', laptopName.value);
-//   localStorage.setItem('laptop-brand', laptopBrand.value);
-//   localStorage.setItem('laptop-cpu', laptopCpu.value);
   localStorage.setItem('cpu-core', cpuCore.value);
   localStorage.setItem('cpu-thread', cpuThread.value);
   localStorage.setItem('ram', ram.value);
@@ -41,6 +34,7 @@ function populateStorage() {
   setInputs();
 }
 
+
 function setInputs() {
   let currentName = localStorage.getItem('name');
   let currentSurname = localStorage.getItem('surname');
@@ -48,66 +42,66 @@ function setInputs() {
   let currentNumber = localStorage.getItem('number');
 
   let currentLaptopName = localStorage.getItem('laptop-name');
-//   let currentLaptopBrand = localStorage.getItem('laptop-brand');
-//   let currentLaptopCpu = localStorage.getItem('laptop-cpu');
   let currentCpuCore = localStorage.getItem('cpu-core');
   let currentCpuThread = localStorage.getItem('cpu-thread');
   let currentRam = localStorage.getItem('ram');
   let currentDate = localStorage.getItem('date');
   let currentPrice = localStorage.getItem('price');
 
+
   nameInput.value = currentName;
   surnameInput.value = currentSurname;
   emailInput.value = currentEmail;
   numberInput.value = currentNumber;
 
- laptopName.value = currentLaptopName;
-//  laptopBrand.value = currentLaptopBrand;
-//  laptopCpu.value = currentLaptopCpu;
- cpuCore.value = currentCpuCore;
- cpuThread.value = currentCpuThread;
- ram.value = currentRam;
- date.value = currentDate;
- price.value = currentPrice;
-
+  laptopName.value = currentLaptopName;
+  cpuCore.value = currentCpuCore;
+  cpuThread.value = currentCpuThread;
+  ram.value = currentRam;
+  date.value = currentDate;
+  price.value = currentPrice;
 }
 
-form.addEventListener('input', populateStorage)
+form.addEventListener('input', populateStorage);
+// -------------
 
 
-// fetching team and position data
+
+// FETCHING TEAM AND POSITION DATA
 const selectTeam = document.querySelector('#teams');
 const selectPos = document.querySelector('#positions');
 
 
 fetch('https://pcfy.redberryinternship.ge/api/teams', {mode: 'cors'})
-        .then(response => response.json())
-        .then(function(response){
-            response.data.forEach(element => {
-                const opt = document.createElement('option');
-                opt.textContent = element.name;
-                selectTeam.appendChild(opt)
-            });
-        });
+.then(response => response.json())
+.then(function(response){
+    response.data.forEach(element => {
+        const opt = document.createElement('option');
+        opt.textContent = element.name;
+        opt.setAttribute('data-teamId', element.id);
+        selectTeam.appendChild(opt)
+    });
+});
 
 
-        fetch('https://pcfy.redberryinternship.ge/api/positions', {mode: 'cors'})
-        .then(response => response.json())
-        .then(function(response){
-            response.data.forEach(element => {
-              selectTeam.addEventListener('change', () => {
-                const index = selectTeam.selectedIndex;
-                if (index == element.team_id) {
-                    const opt = document.createElement('option');
-                    opt.textContent = element.name;
-                    positions.appendChild(opt)
-                }
-            })
-            });
-        });
+fetch('https://pcfy.redberryinternship.ge/api/positions', {mode: 'cors'})
+.then(response => response.json())
+.then(function(response){
+    response.data.forEach(element => {
+      selectTeam.addEventListener('change', () => {
+        const index = selectTeam.selectedIndex;
+        if (index == element.team_id) {
+            const opt = document.createElement('option');
+            opt.textContent = element.name;
+            opt.setAttribute('data-posId', element.id);
+            positions.appendChild(opt)
+        }
+    })
+    });
+});
 
 
-// fetching laptop data
+// FETCHING LAPTOP DATA
 const selectBrand = document.querySelector('#laptop-brands');
 const selectCpu = document.querySelector('#cpus');
 
@@ -117,6 +111,7 @@ fetch('https://pcfy.redberryinternship.ge/api/brands', {mode: 'cors'})
     response.data.forEach(element => {
         const opt = document.createElement('option');
         opt.textContent = element.name;
+        opt.setAttribute('data-brand_id', element.id);
         selectBrand.appendChild(opt)
     });
 });
@@ -142,13 +137,17 @@ const firstTitle = document.querySelector('.first-title');
 const secTitle = document.querySelector('.sec-title');
 
 
-
 nxtBtn.addEventListener('click', () => {
-    firstTab.style.display = 'none';
-    secTab.style.display = 'inherit';
+  firstTab.style.display = 'none';
+  secTab.style.display = 'inherit';
+  firstTitle.style.borderBottom = 'none';
+  secTitle.style.borderBottom = '2px solid black';
+  // if(nameInput.value && surnameInput.value && emailInput.value && numberInput.value) {
     
-    firstTitle.style.borderBottom = 'none';
-    secTitle.style.borderBottom = '2px solid black';
+  // } else {
+  //   alert('შეავსეთ ყველა სავალდებულო ზოლი');
+  // }
+    
 })
 
 prvBtn.addEventListener('click', () => {
@@ -160,26 +159,61 @@ prvBtn.addEventListener('click', () => {
 })
 
 
-// file drop zone
-function dropHandler(ev) {
-  ev.preventDefault();
 
-  if (ev.dataTransfer.items) {
-    [...ev.dataTransfer.items].forEach((item, i) => {
-      if (item.kind === 'file') {
-        const file = item.getAsFile();
-        console.log('es romelis?');
-        const fileName = file.name
-        console.log(`… file[${i}].name = ${file.name}`);
-      }
-    });
-  } else {
-    [...ev.dataTransfer.files].forEach((file, i) => {
-      console.log(`… file[${i}].name = ${file.name}`);
-    });
+function enc(element) {
+
+  let reader = new FileReader();
+    
+  reader.onloadend = function() {
+
+    // set image as background
+    document.getElementById('drop_zone').style.backgroundImage = `url(${reader.result})`;
+    document.getElementById('drop_zone').style.backgroundRepeat = `no-repeat`;
+    document.getElementById('drop_zone').style.backgroundSize = `cover`;
+
   }
 }
 
-function dragOverHandler(ev) {
-  ev.preventDefault();
+
+
+// POST FORM DATA
+
+const fileInput = document.getElementById('laptop_image');
+
+
+
+async function handleFormSubmit(event) {
+	event.preventDefault();
+
+  const form = document.querySelector('form');
+	const url = form.action;
+
+	try {
+		const formData = new FormData(form);
+    formData.append('token', 'ca421d1579a320984bc855b2200566e7');
+    formData.append('team_id', Number(teamsSelect.options[teamsSelect.selectedIndex].dataset.teamid));
+    formData.append('position_id', Number(posSelect.options[posSelect.selectedIndex].dataset.posid));
+    formData.append('laptop_image', fileInput.files[0], "img.jpeg");
+    formData.append('laptop_brand_id', Number(selectBrand.options[selectBrand.selectedIndex].dataset.brand_id));
+    formData.delete('teams');
+    formData.delete('positions');
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData,
+    }).catch(error => console.log(error));
+
+    if (!response.ok) {
+      		const errorMessage = await response.text();
+      		throw new Error(errorMessage);
+    } 
+
+	} catch (error) {
+		console.error(error);
+	}
 }
+
+form.addEventListener("submit", handleFormSubmit);
+
+
+
