8b4ae43cc3a195621527d8bba3cc0997c7617e1e
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-05 05:21:18 +0400

    displaying laptops data

--- get-laptops.js
+++ get-laptops.js
@@ -6,28 +6,32 @@ fetch(`https://pcfy.redberryinternship.ge/api/laptops?token=${token}`)
 .then((response) => {
     response.data.forEach(element => {
         const laptop = document.createElement('div');
-        laptop.classList.add('laptop');
+        const titleContainer = document.createElement('div');
+        const h4 = document.createElement('h4');
+        const h5 = document.createElement('h5');
+        const a = document.createElement('a');
         const name = element.user.name;
         const lastName = element.user.surname;
         const laptopName = element.laptop.name;
-        const image = `https://pcfy.redberryinternship.ge/${element.laptop.image}`;
-        laptop.textContent = name;
-        console.log(image);
+        const img = document.createElement('img');
+        laptop.classList.add('laptop');
+        img.classList.add('laptop-image');
+        titleContainer.classList.add('titleContainer');
+
+
+        img.src = `https://pcfy.redberryinternship.ge/${element.laptop.image}`;
+        h4.textContent = name + ' ' + lastName;
+        h5.textContent = laptopName;
+        a.textContent = 'მეტის ნახვა';
+        a.href = '#';
+
+
+        titleContainer.appendChild(h4);
+        titleContainer.appendChild(h5);
+        titleContainer.appendChild(a);
+        laptop.appendChild(titleContainer);
+        laptop.appendChild(img);
         container.appendChild(laptop);
     });
     console.log(response);
 })
-// const selectTeam = document.querySelector('#teams');
-// const selectPos = document.querySelector('#positions');
-
-
-// fetch('https://pcfy.redberryinternship.ge/api/teams', {mode: 'cors'})
-// .then(response => response.json())
-// .then(function(response){
-//     response.data.forEach(element => {
-//         const opt = document.createElement('option');
-//         opt.textContent = element.name;
-//         opt.setAttribute('data-teamId', element.id);
-//         selectTeam.appendChild(opt)
-//     });
-// });
