6baef96a7082aed7c19bccdb859ef707e854fa00
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-05 13:53:51 +0400

    fetched team, position and laptop brand by IDs

--- laptop.js
+++ laptop.js
@@ -19,19 +19,58 @@ async function getLaptopInfo() {
     }
 }
 
+async function getTeams() {
+    let url = 'https://pcfy.redberryinternship.ge/api/teams';
+
+    try { 
+        let res = await fetch(url);
+
+        return await res.json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+async function getPositions() {
+    let url = 'https://pcfy.redberryinternship.ge/api/positions';
+
+    try { 
+        let res = await fetch(url);
+
+        return await res.json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function getBrands() {
+    let url = 'https://pcfy.redberryinternship.ge/api/brands' ;
+
+    try { 
+        let res = await fetch(url);
+
+        return await res.json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
 async function renderLaptop() {
     let laptop = await getLaptopInfo();
     // user info
     document.querySelector('.name').textContent = laptop.data.user.name + ' ' + laptop.data.user.surname;
-    document.querySelector('.team').textContent = laptop.data.user.team_id;
-    document.querySelector('.position').textContent = laptop.data.user.position_id;;
+    getTeams().then(res => document.querySelector('.team').textContent  = res.data[laptop.data.user.team_id].name);
+    getPositions().then(res => document.querySelector('.position').textContent  = res.data[laptop.data.user.position_id].name);
+
     document.querySelector('.email').textContent = laptop.data.user.email;
     document.querySelector('.number').textContent = laptop.data.user.phone_number;
     img.src = `https://pcfy.redberryinternship.ge/${laptop.data.laptop.image}`;
 
     // laptop info
     document.querySelector('.laptop-name').textContent = laptop.data.laptop.name;
-    document.querySelector('.laptop-brand').textContent = laptop.data.laptop.brand_id;
+    getBrands().then(res => document.querySelector('.laptop-brand').textContent  = res.data[laptop.data.laptop.brand_id].name);
     document.querySelector('.ram').textContent = laptop.data.laptop.ram;
     document.querySelector('.memory-type').textContent = laptop.data.laptop.hard_drive_type;
     document.querySelector('.cpu').textContent = laptop.data.laptop.cpu.name;
@@ -42,8 +81,6 @@ async function renderLaptop() {
     document.querySelector('.price').textContent = laptop.data.laptop.price;
     document.querySelector('.date').textContent = laptop.data.laptop.purchase_date;
     console.log(laptop.data);
-
-
 }
 
 
