0e246947977186994e22ace69d7341b66ead7842
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-11-27 11:26:12 +0400

    reafractored code in get-laptops.js

--- get-all-laptops/get-laptops.js
+++ get-all-laptops/get-laptops.js
@@ -9,37 +9,21 @@ backBtn.addEventListener('click', () => {
 fetch(`https://pcfy.redberryinternship.ge/api/laptops?token=${token}`)
 .then(response => response.json())
 .then((response) => {
+    let html = '';
     response.data.forEach(element => {
-        const laptop = document.createElement('div');
-        const titleContainer = document.createElement('div');
-        const h4 = document.createElement('h4');
-        const h5 = document.createElement('h5');
-        const a = document.createElement('a');
-        const name = element.user.name;
-        const lastName = element.user.surname;
-        const laptopName = element.laptop.name;
-        const id = element.laptop.id;
-        const img = document.createElement('img');
-        laptop.classList.add('laptop');
-        img.classList.add('laptop-image');
-        titleContainer.classList.add('titleContainer');
-
-
-        img.src = `https://pcfy.redberryinternship.ge/${element.laptop.image}`;
-        h4.textContent = name + ' ' + lastName;
-        h5.textContent = laptopName;
-        a.textContent = 'მეტის ნახვა';
-        
-        a.href = `../laptops/laptop.html?${id}`;
-
-        titleContainer.appendChild(h4);
-        titleContainer.appendChild(h5);
-        titleContainer.appendChild(a);
-        laptop.appendChild(titleContainer);
-        laptop.appendChild(img);
-        container.appendChild(laptop);
+        let htmlRender = `
+            <div class="laptop">
+                <div class="titleContainer">
+                    <h4>${element.user.name + ' ' + element.user.surname}</h4>
+                    <h5>${element.laptop.name}</h5>
+                    <a href='../laptops/laptop.html?${element.laptop.id}'>მეტის ნახვა</a>
+                </div>
+                <img class='laptop-image' src='https://pcfy.redberryinternship.ge/${element.laptop.image}'>
+            </div>
+        `;   
+        html += htmlRender;
     });
-    console.log(response);
+    container.innerHTML = html;
 })
 .catch(response => console.log(response));
 
